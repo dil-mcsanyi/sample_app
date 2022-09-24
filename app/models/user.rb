c504091b437b3db5f56cfde6d1f4d59247a663ff
Marcell Csányi <mcsanyi@diligent.com>
2022-09-24 16:55:52 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: true
   validates :password,  presence: true,
-                        length: { minimum: 6 }
+                        length: { minimum: 6 },
+                        allow_nil: true
 
   has_secure_password
 
